afb0bfe2317f2d574bfa990b647f24dad60dd622
soufiane ekouines <93059867+soufiane-ekouines@users.noreply.github.com>
2021-12-13 22:33:27 +0100

    Delete todo directory

--- todo/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import '@fortawesome/fontawesome-free/css/all.css'
-import '@fortawesome/fontawesome-free/js/all.js'
-
-createApp(App).mount('#app')
